fb1239c643a9320b062603d49ff265489658f9e4
KateVolodko <katevolodko04@gmail.com>
2023-05-09 22:38:59 +0500

    timer is added

--- exampleLevelCode.py
+++ exampleLevelCode.py
@@ -1,6 +1,7 @@
-import PyQt5.Qt
-from PyQt5 import QtWidgets, QtCore, Qt
-from PyQt5.Qt import QTextCursor, QColor, QPen, QTextEdit, QTextCharFormat, QFont, QBrush, QFrame
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.Qt import QTextCursor, QColor, QTextEdit, QTextCharFormat, QFont, QBrush, QFrame
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QLabel
 
 from exampleLevel import Ui_MainWindow
 import sys
@@ -121,111 +122,153 @@ class Mywindow(QtWidgets.QMainWindow):
             self.text = f.read()
         self.ui.type_here.setText(self.text)
 
-        self.textEdit.setEnabled(True)
-        self.textEdit.setFocus()
+        self.textEdit.setEnabled(False)
         self.textEdit.setStyleSheet("background-color: rgba(0,0,0,0);"
                                     "color: rgba(0,0,0,0);")
         self.textEdit.setFrameStyle(QFrame.NoFrame)
         self.textEdit.setCursorWidth(0)
         self.pos = -1
-        self.print_letter()
         self.errors = []
-        print(self.ui.type_here.document().toPlainText()[self.pos+1] == " ")
         self.textEdit.document().contentsChange.connect(self.contents_change)
         self.textEdit.document().contentsChange.connect(self.print_letter)
         self.format = QTextCharFormat()
         self.format.setFont(QFont("Roboto", 25, QFont.Bold))
 
-        self.key_labels = {
-            48: self.ui.number_0,
-            49: self.ui.number_1,
-            50: self.ui.number_2,
-            51: self.ui.number_3,
-            52: self.ui.number_4,
-            53: self.ui.number_5,
-            54: self.ui.number_6,
-            55: self.ui.number_7,
-            56: self.ui.number_8,
-            57: self.ui.number_9,
-            16777216: self.ui.key_Esc,
-            16777217: self.ui.key_Tab,
-            16777219: self.ui.key_Backspace,
-            16777223: self.ui.key_Delete,
-            16777222: self.ui.key_Insert,
-            16777220: self.ui.key_Enter,
-            16777234: self.ui.key_left,
-            16777236: self.ui.key_right,
-            16777235: self.ui.key_up,
-            16777237: self.ui.key_down,
-            16777250: self.ui.key_Wind_key,
-            16777252: self.ui.key_CapsLk,
-            16777264: self.ui.key_F1,
-            16777265: self.ui.key_F2,
-            16777266: self.ui.key_F3,
-            16777267: self.ui.key_F4,
-            16777268: self.ui.key_F5,
-            16777269: self.ui.key_F6,
-            16777270: self.ui.key_F7,
-            16777271: self.ui.key_F8,
-            16777272: self.ui.key_F9,
-            16777273: self.ui.key_F10,
-            16777274: self.ui.key_F11,
-            16777275: self.ui.key_F12,
-            32: self.ui.key_space,
-            45: self.ui.key_minus,
-            61: self.ui.key_eq,
-            46: self.ui.key_dot,
-            1025: self.ui.letter_7,
-            1049: self.ui.letter_11,
-            1062: self.ui.letter_24,
-            1059: self.ui.letter_21,
-            1050: self.ui.letter_12,
-            1045: self.ui.letter_6,
-            1053: self.ui.letter_15,
-            1043: self.ui.letter_4,
-            1064: self.ui.letter_26,
-            1065: self.ui.letter_27,
-            1047: self.ui.letter_9,
-            1061: self.ui.letter_23,
-            1066: self.ui.letter_28,
-            1060: self.ui.letter_22,
-            1067: self.ui.letter_29,
-            1042: self.ui.letter_3,
-            1040: self.ui.letter_1,
-            1055: self.ui.letter_17,
-            1056: self.ui.letter_18,
-            1054: self.ui.letter_16,
-            1051: self.ui.letter_13,
-            1044: self.ui.letter_5,
-            1046: self.ui.letter_8,
-            1069: self.ui.letter_31,
-            1071: self.ui.letter_33,
-            1063: self.ui.letter_25,
-            1057: self.ui.letter_19,
-            1052: self.ui.letter_14,
-            1048: self.ui.letter_10,
-            1058: self.ui.letter_20,
-            1068: self.ui.letter_30,
-            1041: self.ui.letter_2,
-            1070: self.ui.letter_32,
-            92: self.ui.key_backslash,
-        }
+        # self.key_labels = {
+        #     48: self.ui.number_0,
+        #     49: self.ui.number_1,
+        #     50: self.ui.number_2,
+        #     51: self.ui.number_3,
+        #     52: self.ui.number_4,
+        #     53: self.ui.number_5,
+        #     54: self.ui.number_6,
+        #     55: self.ui.number_7,
+        #     56: self.ui.number_8,
+        #     57: self.ui.number_9,
+        #     16777216: self.ui.key_Esc,
+        #     16777217: self.ui.key_Tab,
+        #     16777219: self.ui.key_Backspace,
+        #     16777223: self.ui.key_Delete,
+        #     16777222: self.ui.key_Insert,
+        #     16777220: self.ui.key_Enter,
+        #     16777234: self.ui.key_left,
+        #     16777236: self.ui.key_right,
+        #     16777235: self.ui.key_up,
+        #     16777237: self.ui.key_down,
+        #     16777250: self.ui.key_Wind_key,
+        #     16777252: self.ui.key_CapsLk,
+        #     16777264: self.ui.key_F1,
+        #     16777265: self.ui.key_F2,
+        #     16777266: self.ui.key_F3,
+        #     16777267: self.ui.key_F4,
+        #     16777268: self.ui.key_F5,
+        #     16777269: self.ui.key_F6,
+        #     16777270: self.ui.key_F7,
+        #     16777271: self.ui.key_F8,
+        #     16777272: self.ui.key_F9,
+        #     16777273: self.ui.key_F10,
+        #     16777274: self.ui.key_F11,
+        #     16777275: self.ui.key_F12,
+        #     32: self.ui.key_space,
+        #     45: self.ui.key_minus,
+        #     61: self.ui.key_eq,
+        #     46: self.ui.key_dot,
+        #     1025: self.ui.letter_7,
+        #     1049: self.ui.letter_11,
+        #     1062: self.ui.letter_24,
+        #     1059: self.ui.letter_21,
+        #     1050: self.ui.letter_12,
+        #     1045: self.ui.letter_6,
+        #     1053: self.ui.letter_15,
+        #     1043: self.ui.letter_4,
+        #     1064: self.ui.letter_26,
+        #     1065: self.ui.letter_27,
+        #     1047: self.ui.letter_9,
+        #     1061: self.ui.letter_23,
+        #     1066: self.ui.letter_28,
+        #     1060: self.ui.letter_22,
+        #     1067: self.ui.letter_29,
+        #     1042: self.ui.letter_3,
+        #     1040: self.ui.letter_1,
+        #     1055: self.ui.letter_17,
+        #     1056: self.ui.letter_18,
+        #     1054: self.ui.letter_16,
+        #     1051: self.ui.letter_13,
+        #     1044: self.ui.letter_5,
+        #     1046: self.ui.letter_8,
+        #     1069: self.ui.letter_31,
+        #     1071: self.ui.letter_33,
+        #     1063: self.ui.letter_25,
+        #     1057: self.ui.letter_19,
+        #     1052: self.ui.letter_14,
+        #     1048: self.ui.letter_10,
+        #     1058: self.ui.letter_20,
+        #     1068: self.ui.letter_30,
+        #     1041: self.ui.letter_2,
+        #     1070: self.ui.letter_32,
+        #     92: self.ui.key_backslash,
+        # }
+        # self.double_key_labels = [
+        #     (QtCore.Qt.Key_Control, self.ui.key_Ctrl_l),
+        #     (QtCore.Qt.Key_Control, self.ui.key_Ctrl_r),
+        #     (QtCore.Qt.Key_Alt, self.ui.key_Alt_l),
+        #     (QtCore.Qt.Key_Alt, self.ui.key_Alt_r),
+        #     (QtCore.Qt.Key_Shift, self.ui.key_Shift_l),
+        #     (QtCore.Qt.Key_Shift, self.ui.key_Shift_r)]
 
+        self.time_label = QLabel("0:0", self)
+        self.time_label.setGeometry(QtCore.QRect(910, 80, 150, 30))
+        self.time_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.time_label.setStyleSheet("background-color: rgba(255,255,255);"
+                                      "color: rgba(0,0,0);"
+                                      "border-top-left-radius: 15px;"
+                                      "border-bottom-left-radius: 15px;"
+                                      "border-top-right-radius: 15px;"
+                                      "border-bottom-right-radius: 15px;")
+        self.time_label.setFrameStyle(QFrame.NoFrame)
+        self.ui.start.clicked.connect(self.start)
+        self.ui.restart.clicked.connect(self.restart)
 
-        self.double_key_labels = [
-            (QtCore.Qt.Key_Control, self.ui.key_Ctrl_l),
-            (QtCore.Qt.Key_Control, self.ui.key_Ctrl_r),
-            (QtCore.Qt.Key_Alt, self.ui.key_Alt_l),
-            (QtCore.Qt.Key_Alt, self.ui.key_Alt_r),
-            (QtCore.Qt.Key_Shift, self.ui.key_Shift_l),
-            (QtCore.Qt.Key_Shift, self.ui.key_Shift_r)]
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_time)
+        self.second = 0
+        self.count = 0
+        self.right_letters_count = 0
+
+    def update_time(self):
+        self.second += 1
+        self.time_label.setText(f'{self.second // 60}:{self.second % 60}')
+        speed = self.count / self.second
+        self.ui.cur_speed.setText(str(round(speed, 2)))
+
+    def start(self):
+        if not self.timer.isActive():
+            self.textEdit.setEnabled(True)
+            self.textEdit.setFocus()
+            self.ui.start.setText("||")
+            self.timer.start(1000)
+
+        else:
+            self.ui.start.setText("►")
+            self.timer.stop()
+            self.textEdit.setEnabled(False)
+
+    def restart(self):
+        self.timer.stop()
+        self.time_label.setText("0:0")
+        self.ui.start.setText("►")
+        self.ui.cur_speed.setText("0")
+        self.ui.cur_accuracy.setText("0")
+        self.right_letters_count, self.count = 0, 0
+        self.errors = []
+        self.pos = -1
+        self.textEdit.setEnabled(False)
 
     def print_letter(self):
         letter = self.ui.type_here.document().toPlainText()[self.pos+1]
         for i in self.key_label.keys():
             self.key_label[i].setStyleSheet("background-color: white;")
 
-        print(letter)
         if letter.isalpha() and letter.isupper() or letter in self.special_symbols.keys():
             a = letter.lower() if letter.isalpha() else self.special_symbols[letter]
             if a in self.with_right_shift:
@@ -235,8 +278,6 @@ class Mywindow(QtWidgets.QMainWindow):
             self.key_label[a].setStyleSheet("background-color: grey;")
         elif letter.isalpha() and letter.islower() or letter.isdigit() or letter in [" ", "\n", "\t", "."]:
             self.key_label[letter].setStyleSheet("background-color: grey;")
-
-
     '''
     def keyPressEvent(self, event):
         print(event.text())
@@ -259,10 +300,8 @@ class Mywindow(QtWidgets.QMainWindow):
     def contents_change(self, position):
         cursor = self.ui.type_here.textCursor()
         cursor.setPosition(position)
-        end = 0
         if cursor.position() <= self.pos:
-            print(cursor.position(), self.pos)
-            for i in range(-cursor.position()+self.pos, -1, -1 ):
+            for i in range(-cursor.position()+self.pos, -1, -1):
                 cursor.movePosition(QTextCursor.NextCharacter, -1)
                 self.format.setBackground(QBrush(QColor("white")))
                 cursor.mergeCharFormat(self.format)
@@ -273,11 +312,13 @@ class Mywindow(QtWidgets.QMainWindow):
         self.pos = position
         if end:
             letter_text = self.text[position]
+            self.count += 1
             letter_area_for_typing = self.textEdit.document().toPlainText()[position]
             if position in self.errors and letter_text == letter_area_for_typing:
                 self.format.setBackground(QBrush(QColor("yellow")))
                 self.format.setFontWordSpacing(10)
             elif letter_text == letter_area_for_typing:
+                self.right_letters_count += 1
                 self.format.setBackground(QBrush(QColor("green")))
                 self.format.setFontWordSpacing(10)
             else:
@@ -289,7 +330,8 @@ class Mywindow(QtWidgets.QMainWindow):
                 self.textEdit.setEnabled(False)
         else:
             self.textEdit.setEnabled(False)
-
+        accuracy = self.right_letters_count/self.count
+        self.ui.cur_accuracy.setText("{:.2%}".format(accuracy))
         cursor.mergeCharFormat(self.format)
 
 
