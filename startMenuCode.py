4610bf97b61e99a173334b62eb89a01dff81dea7
KateVolodko <katevolodko04@gmail.com>
2023-05-11 09:15:54 +0500

    click for start

--- startMenuCode.py
+++ startMenuCode.py
@@ -18,7 +18,7 @@ class Start_Scene(QtWidgets.QMainWindow):
         self.ui.invisible_button.clicked.connect(self.goto_exercises)
 
     def goto_random(self):
-        random = Keyboard_Scene("TextExample.txt", True)
+        random = Keyboard_Scene("TextExample.txt", True, 0)
         widget.addWidget(random)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -34,21 +34,23 @@ class Exercises_scene(QtWidgets.QMainWindow):
         self.ui = Ui_exercises_scene()
         self.ui.setupUi(self)
 
-        a = "тренажер.txt"
-        self.ui.button_go_home.clicked.connect(self.goto_start_menu)
-        self.ui.button_level_1.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_2.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_3.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_4.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_5.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_6.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_7.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_8.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_9.clicked.connect(lambda : self.goto_keyboard(a))
-        self.ui.button_level_10.clicked.connect(lambda : self.goto_keyboard(a))
+        main_text = "тренажер.txt"
+        extra_text = "для мизинцев.txt"
 
-    def goto_keyboard(self, text):
-        keyboard = Keyboard_Scene("тренажер.txt", False)
+        self.ui.button_go_home.clicked.connect(self.goto_start_menu)
+        self.ui.button_level_1.clicked.connect(lambda: self.goto_keyboard(main_text, 1))
+        self.ui.button_level_2.clicked.connect(lambda: self.goto_keyboard(main_text, 2))
+        self.ui.button_level_3.clicked.connect(lambda: self.goto_keyboard(main_text, 3))
+        self.ui.button_level_4.clicked.connect(lambda: self.goto_keyboard(main_text, 4))
+        self.ui.button_level_5.clicked.connect(lambda: self.goto_keyboard(main_text, 5))
+        self.ui.button_level_6.clicked.connect(lambda: self.goto_keyboard(extra_text, 0))
+        self.ui.button_level_7.clicked.connect(lambda: self.goto_keyboard(main_text, 6))
+        self.ui.button_level_8.clicked.connect(lambda: self.goto_keyboard(main_text, 7))
+        self.ui.button_level_9.clicked.connect(lambda: self.goto_keyboard(main_text, 8))
+        self.ui.button_level_10.clicked.connect(lambda: self.goto_keyboard(main_text, 9))
+
+    def goto_keyboard(self, text, level_number):
+        keyboard = Keyboard_Scene(text, False, level_number)
         widget.addWidget(keyboard)
         widget.setCurrentIndex(widget.currentIndex() + 1)
     
@@ -59,7 +61,7 @@ class Exercises_scene(QtWidgets.QMainWindow):
 
 
 class Keyboard_Scene(QtWidgets.QMainWindow):
-    def __init__(self, text, isFromStartMenu):
+    def __init__(self, exerc_text, isFromStartMenu, level_number):
         super(Keyboard_Scene, self).__init__()
 
         self.ui = Ui_keyboard_scene()
@@ -170,15 +172,19 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         self.textEdit = QTextEdit(self)
         self.textEdit.setGeometry(QtCore.QRect(150, 250, 1580, 250))
         self.text = ""
-        with open(text, encoding="utf-8") as f:
-            self.text = f.read()
+        with open(exerc_text, encoding="utf-8") as f:
+            if level_number != 0:
+                self.text = f.read().split('\n')[level_number - 1]
+            else:
+                self.text = f.read()
         self.ui.type_here.setText(self.text)
 
-        self.textEdit.setEnabled(False)
+        self.textEdit.setEnabled(True)
         self.textEdit.setStyleSheet("background-color: rgba(0,0,0,0);"
                                     "color: rgba(0,0,0,0);")
         self.textEdit.setFrameStyle(QFrame.NoFrame)
         self.textEdit.setCursorWidth(0)
+        self.textEdit.setFocus()
         self.pos = -1
         self.a = 0
         self.rest = False
@@ -189,93 +195,11 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         self.format = QTextCharFormat()
         self.format.setFont(QFont("Roboto", 25, QFont.Bold))
 
-        # self.key_labels = {
-        #     48: self.ui.number_0,
-        #     49: self.ui.number_1,
-        #     50: self.ui.number_2,
-        #     51: self.ui.number_3,
-        #     52: self.ui.number_4,
-        #     53: self.ui.number_5,
-        #     54: self.ui.number_6,
-        #     55: self.ui.number_7,
-        #     56: self.ui.number_8,
-        #     57: self.ui.number_9,
-        #     16777216: self.ui.key_Esc,
-        #     16777217: self.ui.key_Tab,
-        #     16777219: self.ui.key_Backspace,
-        #     16777223: self.ui.key_Delete,
-        #     16777222: self.ui.key_Insert,
-        #     16777220: self.ui.key_Enter,
-        #     16777234: self.ui.key_left,
-        #     16777236: self.ui.key_right,
-        #     16777235: self.ui.key_up,
-        #     16777237: self.ui.key_down,
-        #     16777250: self.ui.key_Wind_key,
-        #     16777252: self.ui.key_CapsLk,
-        #     16777264: self.ui.key_F1,
-        #     16777265: self.ui.key_F2,
-        #     16777266: self.ui.key_F3,
-        #     16777267: self.ui.key_F4,
-        #     16777268: self.ui.key_F5,
-        #     16777269: self.ui.key_F6,
-        #     16777270: self.ui.key_F7,
-        #     16777271: self.ui.key_F8,
-        #     16777272: self.ui.key_F9,
-        #     16777273: self.ui.key_F10,
-        #     16777274: self.ui.key_F11,
-        #     16777275: self.ui.key_F12,
-        #     32: self.ui.key_space,
-        #     45: self.ui.key_minus,
-        #     61: self.ui.key_eq,
-        #     46: self.ui.key_dot,
-        #     1025: self.ui.letter_7,
-        #     1049: self.ui.letter_11,
-        #     1062: self.ui.letter_24,
-        #     1059: self.ui.letter_21,
-        #     1050: self.ui.letter_12,
-        #     1045: self.ui.letter_6,
-        #     1053: self.ui.letter_15,
-        #     1043: self.ui.letter_4,
-        #     1064: self.ui.letter_26,
-        #     1065: self.ui.letter_27,
-        #     1047: self.ui.letter_9,
-        #     1061: self.ui.letter_23,
-        #     1066: self.ui.letter_28,
-        #     1060: self.ui.letter_22,
-        #     1067: self.ui.letter_29,
-        #     1042: self.ui.letter_3,
-        #     1040: self.ui.letter_1,
-        #     1055: self.ui.letter_17,
-        #     1056: self.ui.letter_18,
-        #     1054: self.ui.letter_16,
-        #     1051: self.ui.letter_13,
-        #     1044: self.ui.letter_5,
-        #     1046: self.ui.letter_8,
-        #     1069: self.ui.letter_31,
-        #     1071: self.ui.letter_33,
-        #     1063: self.ui.letter_25,
-        #     1057: self.ui.letter_19,
-        #     1052: self.ui.letter_14,
-        #     1048: self.ui.letter_10,
-        #     1058: self.ui.letter_20,
-        #     1068: self.ui.letter_30,
-        #     1041: self.ui.letter_2,
-        #     1070: self.ui.letter_32,
-        #     92: self.ui.key_backslash,
-        # }
-        # self.double_key_labels = [
-        #     (QtCore.Qt.Key_Control, self.ui.key_Ctrl_l),
-        #     (QtCore.Qt.Key_Control, self.ui.key_Ctrl_r),
-        #     (QtCore.Qt.Key_Alt, self.ui.key_Alt_l),
-        #     (QtCore.Qt.Key_Alt, self.ui.key_Alt_r),
-        #     (QtCore.Qt.Key_Shift, self.ui.key_Shift_l),
-        #     (QtCore.Qt.Key_Shift, self.ui.key_Shift_r)]
-
         self.time_label = QLabel("0:0", self)
         self.time_label.setGeometry(QtCore.QRect(910, 80, 150, 30))
         self.time_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.time_label.setStyleSheet("background-color: rgba(255,255,255);"
-                                      "color: rgba(0,0,0);"
+        self.time_label.setStyleSheet("background-color: white;"
+                                      "color: black;"
                                       "border-top-left-radius: 15px;"
                                       "border-bottom-left-radius: 15px;"
                                       "border-top-right-radius: 15px;"
@@ -289,6 +213,7 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         self.second = 0
         self.count = 0
         self.right_letters_count = 0
+        self.is_stopped = True
 
     def update_time(self):
         self.second += 1
@@ -297,19 +222,12 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         self.ui.cur_speed.setText(str(round(speed, 2)))
 
     def start(self):
-        if not self.timer.isActive():
-            self.textEdit.setEnabled(True)
-            self.textEdit.setFocus()
-            self.ui.start.setText("||")
-            self.timer.start(1000)
-
-        else:
-            self.ui.start.setText("►")
-            self.timer.stop()
-            self.textEdit.setEnabled(False)
+        self.is_stopped = True
+        self.ui.start.setText("►")
+        self.timer.stop()
 
     def restart(self):
-        self.rest = True
+        self.is_stopped = True
         self.timer.stop()
         self.time_label.setText("0:0")
         self.ui.start.setText("►")
@@ -321,7 +239,6 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         self.a = 0
         self.ui.type_here.verticalScrollBar().setValue(0)
         self.pos = -1
-        self.textEdit.setEnabled(False)
 
     def print_letter(self):
         letter = self.ui.type_here.document().toPlainText()[self.pos+1]
@@ -337,26 +254,12 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
             self.key_label[a].setStyleSheet("background-color: grey;")
         elif letter.isalpha() and letter.islower() or letter.isdigit() or letter in [" ", "\n", "\t", "."]:
             self.key_label[letter].setStyleSheet("background-color: grey;")
-    '''
-    def keyPressEvent(self, event):
-        print(event.text())
-        for key, label in self.double_key_labels:
-            if event.key() == key:
-                label.setStyleSheet("background-color: gray;")
-        if event.key() in self.key_labels:
-            self.key_labels[event.key()].setStyleSheet("background-color: gray;")
-        event.accept()
-
-    def keyReleaseEvent(self, event):
-        for key, label in self.double_key_labels:
-            if event.key() == key:
-                label.setStyleSheet("background-color: transparent;")
-        if event.key() in self.key_labels:
-            self.key_labels[event.key()].setStyleSheet("background-color: transparent;")
-        event.accept()
-    '''
 
     def contents_change(self, position):
+        if self.textEdit.document().toPlainText() != "" and self.is_stopped:
+            self.ui.start.setText("||")
+            self.timer.start(1000)
+            self.is_stopped = False
         cursor = self.ui.type_here.textCursor()
         cursor.setPosition(position)
         if cursor.position() <= self.pos or self.textEdit.document().toPlainText() == "":
@@ -404,7 +307,6 @@ class Keyboard_Scene(QtWidgets.QMainWindow):
         prev = Start_Scene() if self.isFromStartMenu else Exercises_scene()
         widget.addWidget(prev)
         widget.setCurrentIndex(widget.currentIndex() + 1)
- 
 
 app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
