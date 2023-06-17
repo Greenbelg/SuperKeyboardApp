dc5506c9bfa84268dc3af12924a0de7f6de8badd
KateVolodko <katevolodko04@gmail.com>
2023-06-17 19:14:29 +0500

    add random text genres and punctuation tests

--- Tests.py
+++ Tests.py
@@ -25,6 +25,56 @@ class TestTest(unittest.TestCase):
         self._check_stat("levels", ["180", "89", "350"])
         folder_statistics_path.joinpath("levels_stat.txt").read_text()
         folder_statistics_path.joinpath("levels_stat.txt").write_text(old_stat)
+    
+    def test_simple_punctuation(self):
+        test_text = "Да замолчи ты! Зачем такие вещи рассказывать? Старик покраснел и извинился."
+        correct_split = ['Да замолчи ты!',
+                         'Зачем такие вещи рассказывать?',
+                         'Старик покраснел и извинился.']
+        self.assertEqual(correct_split, startMenuCode.split_text(test_text))
+    
+    def test_split_sent_with_enter(self):
+        test_text = 'Он не любил, чтобы к нему ходили.\nТак прошел год,' \
+                    ' по окончании которого с Герасимом случилось небольшое происшествие.'
+        correct_split = ['Он не любил, чтобы к нему ходили.',
+                         'Так прошел год, по окончании которого '
+                         'с Герасимом случилось небольшое происшествие.']
+        self.assertEqual(correct_split, startMenuCode.split_text(test_text))
+    
+    def test_initials(self):
+        test_text = 'Речь идет об историческом четырехтомном романе' \
+                    ' «Стрельцы» К.П.Масальского (1802–1861),' \
+                    ' вышедшем в свет в 1832 году.'
+        correct_split = ['Речь идет об историческом четырехтомном романе'
+                         ' «Стрельцы» К.П.Масальского (1802–1861),'
+                         ' вышедшем в свет в 1832 году.']
+        self.assertEqual(correct_split, startMenuCode.split_text(test_text))
 
+    def test_complex_punctuation(self):
+        test_text = '"А ты небось лучше?" - подумал он про себя.'\
+                    ' Да!.. пусть посватает Татьяну,- решила барыня,' \
+                    ' с удовольствием понюхивая табачок,- слышишь?' \
+                    ' Душевно рад,– начал он,' \
+                    '– и благодарен за доброе намерение посетить нас;' \
+                    ' надеюсь… позвольте узнать ваше имя и отчество?'
+        correct_split = ['"А ты небось лучше?" - подумал он про себя.',
+                         'Да!.. пусть посватает Татьяну,- решила барыня,'
+                         ' с удовольствием понюхивая табачок,- слышишь?',
+                         'Душевно рад,– начал он,– и благодарен за доброе намерение посетить нас; '
+                         'надеюсь… позвольте узнать ваше имя и отчество?']
+        self.assertEqual(correct_split, startMenuCode.split_text(test_text))
+    
+    def test_text_chunk_len(self):
+        test_text = "Зайка сидит в витрине. " \
+        "Он в серенькой рубашке из плюша. " \
+        "Сделали серому зайке cлишком большие уши. " \
+        "В плюшевой шубке серой cидит он, прижавшись к раме. " \
+        "Ну как тут казаться храбрым c такими большими ушами?"
+        splitted_text = startMenuCode.split_text(test_text)
+        max_len = 100
+        curr_len = len(startMenuCode.get_text_chunk(splitted_text,
+                                                    "Зайка сидит в витрине.",
+                                                    max_len))
+        assert curr_len <= max_len
 if __name__ == "__main__":
     unittest.main()
